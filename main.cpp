b8d450110720c3fcbb7f7425e658aee7bf93de4f
Josh Klontz <jklontz@mitre.org>
2012-11-08 16:32:49 -0500

    more code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,60 @@
 #include <QFile>
 #include <QVector>
 
+struct Count
+{
+    int value, count;
+    Count() : value(-1), count(-1) {}
+    Count(int _value, int _count) : value(_value), count(_count) {}
+};
+
+static void search(const QBitArray &data, int bits, Count &most, Count &least)
+{
+    QVector<int> occurences(1 << bits, 0);
+    const int mask = (1 << bits) - 1;
+
+    int index = 0;
+    for (int i=0; i<bits-1; i++)
+        if (data.at(i)) index += 1 << (bits-i-1);
+
+    for (int i=0; i<data.size()-bits; i++) {
+        index = ((index << 1) & mask) + (data.at(i + bits - 1) ? 1 : 0);
+        occurences[index]++;
+    }
+
+    int mostCommonValue = -1;
+    int mostCommonCount = 0;
+    int leastCommonValue = -1;
+    int leastCommonCount = data.size();
+    for (int i=0; i<occurences.size(); i++) {
+        if (occurences[i] > mostCommonCount) {
+            mostCommonCount = occurences[i];
+            mostCommonValue = i;
+        }
+        if (occurences[i] < leastCommonCount) {
+            leastCommonCount = occurences[i];
+            leastCommonValue = i;
+        }
+    }
+
+    most = Count(mostCommonValue, mostCommonCount);
+    least = Count(leastCommonValue, leastCommonCount);
+}
+
+Count mostCommon(const QBitArray &data, int bits)
+{
+    Count most, least;
+    search(data, bits, most, least);
+    return most;
+}
+
+Count leastCommon(const QBitArray &data, int bits)
+{
+    Count most, least;
+    search(data, bits, most, least);
+    return least;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
@@ -21,34 +75,17 @@ int main(int argc, char *argv[])
         for (int j=0; j<8; j++)
             data.setBit(i*8+j, byteArray[i] & (1 << j));
 
-    int escapeWidth = 0; //18; //0;
-    int escapeValue = -1; //106484; //-1;
-    int compressCount = 0;
-    int compressValue = -1;
-    while (escapeValue == -1) {
-        escapeWidth++;
-        QVector<int> occurences(1 << escapeWidth, 0);
-        for (int i=0; i<data.size()-escapeWidth; i++) {
-            int index = 0;
-            for (int j=0; j<escapeWidth; j++)
-                if (data.at(i+j)) index += 1 << j;
-            occurences[index]++;
-        }
-
-        compressCount = 0;
-        compressValue = -1;
-        for (int i=0; i<occurences.size(); i++) {
-            if (occurences[i] == 0) {
-                escapeValue = i;
-            } else if (occurences[i] > compressCount) {
-                compressCount = occurences[i];
-                compressValue = i;
-            }
-        }
+    int bits = 0;
+    Count least;
+    while (least.count != 0) {
+        bits++;
+        least = leastCommon(data, bits);
     }
 
-    qDebug("Escape value = %d (%d bits)", escapeValue, escapeWidth);
-    qDebug("Compress value = %d (%d times)", compressValue, compressCount);
+    qDebug("Zero value = %d (%d bits)", least.value, bits);
+
+    Count most = mostCommon(data, bits);
+    qDebug("Most value = %d (%d times)", most.value, most.count);
 
     return 0;
 }
