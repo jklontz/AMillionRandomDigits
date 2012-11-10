ddf62efd1d978c0858d8237b97a0cb8c71066961
Josh Klontz <jklontz@mitre.org>
2012-11-10 10:58:39 -0500

    code cleanup

--- main.cpp
+++ main.cpp
@@ -6,45 +6,22 @@
 
 struct Number
 {
-    uint value, bits;
+    quint64 value;
+    quint8 bits;
     Number() : value(0), bits(0) {}
-    Number(uint _value, uint _bits) : value(_value), bits(_bits) {}
+    Number(quint64 _value, quint8 _bits) : value(_value), bits(_bits) {}
     Number(const QBitArray &bitArray)
     {
         bits = bitArray.size();
         value = 0;
-        for (int i=0; i<bits; i++)
+        for (quint8 i=0; i<bits; i++)
             if (bitArray.at(i)) value += 1 << (bits-i-1);
     }
 
-    operator QBitArray() const
-    {
-        QBitArray result(bits);
-        for (int i=0; i<bits; i++)
-            result.setBit(value & (1 << bits-i-1), i);
-        return result;
-    }
-
-    inline bool operator[](int index) const
-    {
-        return value & (1 << (bits-index-1));
-    }
-
-    inline operator uint() const
-    {
-        return value;
-    }
-
-    inline void shift(bool nextVal)
-    {
-        value = ((value << 1) & ((ulong(1) << bits) - 1)) + (nextVal ? 1 : 0);
-    }
-
-    inline Number &operator<<(bool nextVal)
-    {
-        shift(nextVal);
-        return *this;
-    }
+    inline bool operator[](int index) const { return value & (1 << (bits-index-1)); }
+    inline operator uint() const { return value; }
+    inline void shift(bool nextVal) { value = ((value << 1) & ((quint64(1) << bits) - 1)) + nextVal; }
+    inline Number &operator<<(bool nextVal) { shift(nextVal); return *this; }
 };
 
 QDebug operator<<(QDebug dbg, const Number &n)
@@ -56,29 +33,29 @@ QDebug operator<<(QDebug dbg, const Number &n)
 struct Count
 {
     Number number;
-    int count;
+    qint32 count;
     Count() : count(-1) {}
-    Count(const Number &_number, uint _count) : number(_number), count(_count) {}
+    Count(const Number &_number, qint32 _count) : number(_number), count(_count) {}
 };
 
-static void count(const QBitArray &data, uint bits, Count &most, Count &least)
+static void count(const QBitArray &data, quint8 bits, Count &most, Count &least)
 {
     Number index(0, bits);
-    for (int i=0; i<bits-1; i++)
+    for (quint8 i=0; i<bits-1; i++)
         index << data.at(i);
 
-    const uint size = (ulong(1) << bits) - 1;
-    uchar *counts = new uchar[size];
+    const quint64 size = quint64(1) << bits;
+    quint8 *counts = new quint8[size];
     memset(counts, 0, size);
 
-    for (uint i=0; i<data.size()-bits; i++) {
+    for (quint64 i=0; i<data.size()-bits; i++) {
         index << data.at(i + bits - 1);
         if (counts[index] < 255) counts[index]++;
     }
 
     most = Count(Number(), 0);
     least = Count(Number(), 255);
-    for (uint i=0; i<size; i++) {
+    for (quint64 i=0; i<size; i++) {
         if (counts[i] > most.count) most = Count(Number(i, bits), counts[i]);
         if (counts[i] < least.count) least = Count(Number(i, bits), counts[i]);
     }
@@ -86,17 +63,14 @@ static void count(const QBitArray &data, uint bits, Count &most, Count &least)
     delete[] counts;
 }
 
-static Count mostCommon(const QBitArray &data, int bits)
+static Count leastCommon(const QBitArray &data)
 {
+    uint bits = 0;
     Count most, least;
-    count(data, bits, most, least);
-    return most;
-}
-
-static Count leastCommon(const QBitArray &data, int bits)
-{
-    Count most, least;
-    count(data, bits, most, least);
+    while (least.count != 0) {
+        bits++;
+        count(data, bits, most, least);
+    }
     return least;
 }
 
@@ -107,30 +81,25 @@ static QBitArray compress(const QBitArray &data, const Number &before, const Num
     Number afterSize(after.bits, 5);
     QBitArray result(data.size() + beforeSize.bits + before.bits);
 
-    int resultIndex = 0;
-    for (int i=0; i<beforeSize.bits; i++)
-        result.setBit(resultIndex + i, beforeSize[i]);
+    quint64 resultIndex = 0;
+    for (quint8 i=0; i<beforeSize.bits; i++) result.setBit(resultIndex + i, beforeSize[i]);
     resultIndex += beforeSize.bits;
-    for (int i=0; i<before.bits; i++)
-        result.setBit(resultIndex + i, before[i]);
+    for (quint8 i=0; i<before.bits; i++) result.setBit(resultIndex + i, before[i]);
     resultIndex += before.bits;
-    for (int i=0; i<afterSize.bits; i++)
-        result.setBit(resultIndex + i, afterSize[i]);
+    for (quint8 i=0; i<afterSize.bits; i++) result.setBit(resultIndex + i, afterSize[i]);
     resultIndex += afterSize.bits;
-    for (int i=0; i<after.bits; i++)
-        result.setBit(resultIndex + i, after[i]);
+    for (quint8 i=0; i<after.bits; i++) result.setBit(resultIndex + i, after[i]);
     resultIndex += after.bits;
 
     Number index(0, bits);
-    for (int i=0; i<bits-1; i++)
+    for (quint8 i=0; i<bits-1; i++)
         index << data.at(i);
 
-    int i = 0;
+    quint64 i = 0;
     while (i < data.size()-bits) {
         index << data.at(i + bits - 1);
         if (index == before) {
-            for (int j=0; j<after.bits; j++)
-                result.setBit(resultIndex+j, after[j]);
+            for (quint8 j=0; j<after.bits; j++) result.setBit(resultIndex+j, after[j]);
             resultIndex += after.bits;
             i += before.bits;
         } else {
@@ -146,23 +115,18 @@ static QBitArray compress(const QBitArray &data, const Number &before, const Num
 
 static QBitArray tryCompress(const QBitArray &data)
 {
-    uint j = 0;
-    Count least;
-    while (least.count != 0) {
-        j++;
-        least = leastCommon(data, j);
-    }
-
-    int closest = data.size();
-    for (int i=32; i>j; i--) {
-        Count most = mostCommon(data, i);
+    qint32 closest = data.size();
+    for (quint8 i=32; i>0; i--) {
+        Count most, least;
+        count(data, i, most, least);
+        least = leastCommon(compress(data, most.number, Number()));
+        if (least.number.bits >= most.number.bits) break;
         QBitArray compressed = compress(data, most.number, least.number);
-        if (compressed.size() < data.size())
-            return compressed;
-        closest = std::min(closest, compressed.size()-data.size());
+        if (compressed.size() < data.size()) return compressed;
+        closest = qMin(closest, compressed.size()-data.size());
     }
 
-    qDebug("Could not compress, got as close as %d :(", closest);
+    qDebug("Could not compress, got as close as %d bits", closest);
     return data;
 }
 
